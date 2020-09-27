60f35926a04a7f9a5607a6f9522350abe4b4103e
Tareke Tewele <tonihinal@gmail.com>
2020-09-27 09:40:33 +0200

    this is unnessary file for the system

---
+++ yeah.py
@@ -0,0 +1,17 @@
+  numbers =[8,9,34,67,77,2]
+  numbers1 =[0,50,30,44,05]
+numbers.sort()
+print(numbers.sort())
+numbers1.reverse()
+ print(numbers1.reverse())
+
+      secret code =8
+    Limit =3
+   count  =0
+   while count < limit:
+             Toni =input(" accept from keyboard to gess")
+			 if secret code == toni
+			 print(" corectly")
+			  break
+	print(" Sorry faild with number of  trail")
+C:\Users\ToNi HB\AppData\Roaming\Microsoft\Windows\Start Menu
