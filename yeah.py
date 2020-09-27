b51feaeb8796076eb407e942fb52c5da275c0298
Tareke Tewele <tonihinal@gmail.com>
2020-09-27 10:51:47 +0200

    Yeah file update

---
+++ yeah.py
@@ -0,0 +1,16 @@
+  numbers =[8,9,34,67,77,2]
+  numbers1 =[0,50,30,44,05]
+numbers.sort()
+print(numbers.sort())
+numbers1.reverse()
+ print(numbers1.reverse())
+
+      secret code =8
+    Limit =3
+   count  =0
+   while count < limit:
+             Toni =input(" accept from keyboard to gess")
+			 if secret code == Toni
+			 print(" corectly")
+			  break
+	print(" Sorry faild with number of  trail")
